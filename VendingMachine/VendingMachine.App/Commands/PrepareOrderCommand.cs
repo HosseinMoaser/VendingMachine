2a7f884fcc1afcf84d478ddb9db271a0d8a55aef
Hossein Moaser <moaser.hossein@gmail.com>
2022-06-18 17:58:43 +0430

    Some Clean up on codes

--- VendingMachine/VendingMachine.App/Commands/PrepareOrderCommand.cs
+++ VendingMachine/VendingMachine.App/Commands/PrepareOrderCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using VendingMachine.App.ProductsTools;
 using VendingMachine.App.Stores;
 using VendingMachine.App.ViewModels;
 using VendingMachine.Domain.Services;
@@ -32,53 +33,17 @@ namespace VendingMachine.App.Commands
         {
             ICommand backToHomeCommand = new BackFromModalCommand(_modalNavigationStore);
             ICommand cancelOrderCommand = new CancelOrderCommand(_modalNavigationStore);
-            BaseProductServices productServices = GetService(_productsListingViewModel.SelectedProductsListingItemViewModel.ProductName);
+            BaseProductServices productServices = ProductTools.GetService(_productsListingViewModel.SelectedProductsListingItemViewModel.ProductName);
             OrderPageViewModel orderViewModel = new OrderPageViewModel(cancelOrderCommand, backToHomeCommand, productServices)
             {
                 ImageSource = new BitmapImage(new Uri(Directory.GetCurrentDirectory() +
                 _productsListingViewModel.SelectedProductsListingItemViewModel.ImageName, UriKind.Relative)),
                 ProductName = _productsListingViewModel.SelectedProductsListingItemViewModel.ProductName,
-                Steps = GetSteps(_productsListingViewModel.SelectedProductsListingItemViewModel.ProductName),
+                Steps = ProductTools.GetSteps(_productsListingViewModel.SelectedProductsListingItemViewModel.ProductName),
                 IsCancelButtonVisible = true
             };
             _modalNavigationStore.CurrentViewModel = orderViewModel;
         }
 
-
-        private ObservableCollection<string> GetSteps(string productName)
-        {
-            switch (productName.ToLower())
-            {
-                case "hot chocolate":
-                    return new ObservableCollection<string>()  { "Boil Water", "Add drinking chocolate to cup", "Add water" };
-                case "lemon tea":
-                    return new ObservableCollection<string>() { "Boil Water", "Add water" , "Steep tea bag", "Add lemon" };
-                case "white coffee":
-                    return new ObservableCollection<string>() { "Boil Water", "Add sugar", "Add coffee granules ", "Add water", "Add milk"};
-                case "iced coffee":
-                    return new ObservableCollection<string>() { "Crush Ice", "Add ice to blender", "Add coffee syrup to blender", "Blend ingredients", "Add ingredients" };
-                default:
-                    return new ObservableCollection<string>() { "Boil Water", "•	Add drinking chocolate to cup", "•	Add water" };
-            }
-        }
-
-        private BaseProductServices GetService(string productName)
-        {
-            switch (productName.ToLower())
-            {
-                case "hot chocolate":
-                    return new HotChocolateServices();
-                case "lemon tea":
-                    return new LemonTeaServices();
-                case "white coffee":
-                    return new WhiteCoffeeServices();
-                case "iced coffee":
-                    return new IcedCoffeeServices();
-                default:
-                    return new BaseProductServices();
-            }
-        }
-
-    
     }
 }
---
+++ VendingMachine/VendingMachine.App/ProductsTools/ProductTools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.Domain.Services;
+
+namespace VendingMachine.App.ProductsTools
+{
+    public static class ProductTools
+    {
+        public static ObservableCollection<string> GetSteps(string productName)
+        {
+            switch (productName.ToLower())
+            {
+                case "hot chocolate":
+                    return new ObservableCollection<string>() { "Boil Water", "Add drinking chocolate to cup", "Add water" };
+                case "lemon tea":
+                    return new ObservableCollection<string>() { "Boil Water", "Add water", "Steep tea bag", "Add lemon" };
+                case "white coffee":
+                    return new ObservableCollection<string>() { "Boil Water", "Add sugar", "Add coffee granules ", "Add water", "Add milk" };
+                case "iced coffee":
+                    return new ObservableCollection<string>() { "Crush Ice", "Add ice to blender", "Add coffee syrup to blender", "Blend ingredients", "Add ingredients" };
+                default:
+                    return new ObservableCollection<string>() { "Boil Water", "•	Add drinking chocolate to cup", "•	Add water" };
+            }
+        }
+
+        public static BaseProductServices GetService(string productName)
+        {
+            switch (productName.ToLower())
+            {
+                case "hot chocolate":
+                    return new HotChocolateServices();
+                case "lemon tea":
+                    return new LemonTeaServices();
+                case "white coffee":
+                    return new WhiteCoffeeServices();
+                case "iced coffee":
+                    return new IcedCoffeeServices();
+                default:
+                    return new BaseProductServices();
+            }
+        }
+        
+    }
+}
